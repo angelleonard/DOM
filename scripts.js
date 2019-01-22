4a3ba0d1ab2c5e3d92f94ace378be445c96f71bf
angelleonard <aleonard0215@aol.com>
2019-01-22 09:51:01 -0600

    changes

--- scripts.js
+++ scripts.js
@@ -1,14 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Create a buttton
     let button = document.createElement("button");
     let t = document.createTextNode("Add Square");
     button.append(t);
     document.body.appendChild(button);
-    button.addEventListener('click', function (){
+
+    // Make buttton add boxes
+    button.addEventListener('click', function () {
         let div = document.createElement("div");
         div.className = ("boxes")
-        document.body.appendChild(div); 
+        document.body.appendChild(div);
+
+        // Count the boxes
         let x = div.childElementCount;
         console.log(x)
-        let count = document.getElementById('count');    
-    })    
-});
+        let count = document.getElementById('count');
+    
+        // Display number on boxes
+    div.addEventListener("mouseover", function () {
+        let number = document.createTextNode("1");
+        div.append(number);
+        number.className = ("number")
+     })
+    })
+
+})
