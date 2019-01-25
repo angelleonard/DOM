98057f67b7d97a7a60d55af11763edacc30b1a5b
angelleonard <aleonard0215@aol.com>
2019-01-25 12:45:47 -0600

    changes

--- scripts.js
+++ scripts.js
@@ -5,23 +5,50 @@ document.addEventListener('DOMContentLoaded', function () {
     button.append(t);
     document.body.appendChild(button);
 
+    let number = 0;
+
     // Make buttton add boxes
     button.addEventListener('click', function () {
-        let div = document.createElement("div");
+        let div = document.createElement("box");
         div.className = ("boxes")
         document.body.appendChild(div);
+        // Add numbers to boxes
+        let boxText = document.createTextNode(number);
+        div.append(boxText);
+        div.id = number;
+        number++;
 
-        // Count the boxes
-        let x = div.childElementCount;
-        console.log(x)
-        let count = document.getElementById('count');
-    
-        // Display number on boxes
-    div.addEventListener("mouseover", function () {
-        let number = document.createTextNode("1");
-        div.append(number);
-        number.className = ("number")
-     })
+
+        function getRandomColor() {
+            let colors = ['red', 'blue', 'green', 'yellow', 'orange', 'pink', 'purple'];
+            let randomIndex = Math.floor(Math.random() * colors.length);
+            return colors[randomIndex];
+        };
+        div.addEventListener("click", function () {
+            div.style.backgroundColor = getRandomColor();
+
+        })
+        // document.getElementById('number')
+        // div.addEventListener("doubleclick", function removeSquare() {
+        //     if ('number' % 2 > 0) {
+        //         removeChild('div')}
+        //     })
+
+
+                // Display number on boxes
+                div.addEventListener("mouseover", function () {
+                    div.style.color = "white"
+                    div.style.textAlign = "center";
+                })
+                div.addEventListener("mouseout", function () {
+                    div.style.color = "black";
+                    //removeChild('number')
+                })
+
+
+
+            })
+
+
     })
 
-})
